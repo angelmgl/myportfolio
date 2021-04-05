189ce2e6f169645ccca6374d41db453e9a7a1216
Angel Mgl <angelemegeele@gmail.com>
2021-04-05 18:13:56 -0400

    Profile card created

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,11 +4,27 @@ const Layout = ({ children, dark, setDark }) => {
             style={{
                 color: dark ? "#e2e2f2" : "#1e1f26",
                 backgroundColor: dark ? "#1e1f26" : "#e2e2f2",
-                minHeight: '100vh'
             }}
+            className="outerContainer"
         >
-            <main>{children}</main>
             <button onClick={() => setDark(!dark)}>Change theme</button>
+            <main>{children}</main>
+
+            <style jsx>{`
+                .outerContainer {
+                    min-height: 100vh;
+                }
+
+                main {
+                    width: 100%;
+                    max-width: 1000px;
+                    margin: 0 auto;
+                }
+
+                button {
+                    position: absolute;
+                }
+            `}</style>
         </div>
     );
 };
---
+++ components/Profile/index.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import styles from "./Profile.module.css";
+
+const Profile = () => {
+    return (
+        <div className={styles.container}>
+            <Image
+                className={styles.photo}
+                src="/img/profile.png"
+                alt="Angel Alemany"
+                width={250}
+                height={250}
+            />
+            <div>
+                <h1>Angel Alemany</h1>
+                <h2>Frontend Web Developer</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
