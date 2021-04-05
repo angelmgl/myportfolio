991da055aeceb7db687e6d74da3a7e48072c849c
Angel Mgl <angelemegeele@gmail.com>
2021-04-05 14:15:21 -0400

    Initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import React, { useState } from 'react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [dark, setDark] = useState(false)
+
+  return (
+    <Component {...pageProps} dark={dark} setDark={setDark} />
+  )
 }
 
 export default MyApp
