8092a47cdbc2ef699a2f0d2894d8bba95730e78a
Angel Mgl <angelemegeele@gmail.com>
2021-04-06 00:12:46 -0400

    Open profile picture fixture added

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,15 +1,31 @@
 import Image from "next/image";
+import React, { useState } from 'react'
+import { FaTimesCircle } from "react-icons/fa"
 import styles from "./Profile.module.css";
 
 const Profile = () => {
+    const [isPicOpen, setIsPicOpen] = useState(false);
+
     return (
         <div className={styles.container}>
+            <div className={styles.modal}
+            style={{display: isPicOpen ? "block" : "none"}}>
+                <Image
+                    src="/img/profile.png"
+                    alt="Angel Alemany"
+                    layout="fill"
+                    objectFit="cover"
+                />
+                <FaTimesCircle className={styles.close}
+                onClick={() => setIsPicOpen(false)} />
+            </div>
             <Image
                 className={styles.photo}
                 src="/img/profile.png"
                 alt="Angel Alemany"
                 width={250}
                 height={250}
+                onClick={() => setIsPicOpen(true)}
             />
             <div>
                 <h1>Angel Alemany</h1>
