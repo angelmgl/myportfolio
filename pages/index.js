b3a2cc3d28a0e5239190f8fb87fba7dad9dbd18d
Angel Mgl <angelemegeele@gmail.com>
2021-04-06 18:44:00 -0400

    Local merge

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import About from "../components/About";
 import Info from "../components/Info";
 import Layout from "../components/Layout";
@@ -8,6 +9,25 @@ import Skills from "../components/Skills";
 export default function Home({ dark, setDark }) {
     return (
         <Layout dark={dark}>
+            <Head>
+                <title>Angel Alemany, Frontend Web Developer</title>
+                <meta
+                    name="description"
+                    content="Hi there! I'm a frontend web developer from Capiatá, Paraguay. Check out my work!"
+                />
+                <meta name="robots" content="index, follow" />
+                <meta
+                    property="og:title"
+                    content="Take a look at my portfolio!"
+                />
+                <meta property="og:image" content="/img/profile.png" />
+                <meta property="og:type" content="website" />
+                <meta
+                    property="og:description"
+                    content="Hi there! I'm a frontend web developer from Capiatá, Paraguay. Check out my work!"
+                />
+                <meta name="theme-color" content="#183e77" />
+            </Head>
             <Profile />
             <Info />
             <Nav dark={dark} setDark={setDark} />
