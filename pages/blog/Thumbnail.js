4bd0a9f5ea5e30dc83395736425f0d2665330e75
Angel Mgl <angelemegeele@gmail.com>
2021-04-15 12:38:14 -0400

    Bug fix

--- pages/blog/Thumbnail.js
+++ pages/blog/Thumbnail.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-const Thumbnail = ({ title, url, image, description, date, tags }) => {
+const Thumbnail = ({ title, url, image, description, date, tags = [] }) => {
     return (
         <Link href={`/blog/${url}`}>
             <article>
